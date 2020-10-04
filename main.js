2f54917d5e0ab1c0233772a6814c58c6af9e3b96
Karolina Kulinska <karolina.kulinska89@gmail.com>
2020-10-04 15:37:43 +0200

    add function for btnClose thanks to we can close our popUp

--- main.js
+++ main.js
@@ -2,8 +2,19 @@ const btnModal = document.querySelector('.btn-form')
 const popUp = document.querySelector('.modal-wrap')
 const mainContainer = document.querySelector('.container')
 
+// Open Popup
 
 btnModal.addEventListener('click', function () {
     popUp.classList.add('modal-wrap--active')
     mainContainer.classList.add('container--blur')
+})
+
+
+// Close PopUp
+
+const btnClose = document.querySelector('.btn-close')
+
+btnClose.addEventListener('click', function(){
+    popUp.classList.remove('modal-wrap--active')
+    mainContainer.classList.remove('container--blur')
 })
